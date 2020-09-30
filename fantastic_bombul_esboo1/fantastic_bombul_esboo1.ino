6fc3b0839db53acc56f7327c4885379eb06bb5f8
Mall6oro <72058969+Mall6oro@users.noreply.github.com>
2020-09-30 15:56:46 +0500

    Update fantastic_bombul_esboo1.ino

--- fantastic_bombul_esboo1/fantastic_bombul_esboo1.ino
+++ fantastic_bombul_esboo1/fantastic_bombul_esboo1.ino
@@ -1,12 +1,9 @@
 void setup()
 {
-  pinMode(13, OUTPUT);
+pinMode();
 }
 
 void loop()
 {
-  digitalWrite(13, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(13, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+
 }
